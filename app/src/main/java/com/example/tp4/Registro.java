408db9132213c536867a9bc700d6f4c0f375f0c8
Nairan Parody <nairanparody@gmail.com>
2021-06-29 22:03:40 -0300

    Shared Preference

--- app/src/main/java/com/example/tp4/Registro.java
+++ app/src/main/java/com/example/tp4/Registro.java
@@ -2,7 +2,9 @@ package com.example.tp4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,7 @@ public class Registro extends AppCompatActivity {
 
     private Button btnRegistroGuardar;
     private TextView edtRegistroNombre, edtRegistroUsuario, edtRegistroCorreo, edtRegistroClave, edtRegistroClave2;
+    private SharedPreferences pref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +31,27 @@ public class Registro extends AppCompatActivity {
         edtRegistroClave = findViewById(R.id.edtRegistroClave);
         edtRegistroClave2 = findViewById(R.id.edtRegistroClave2);
 
+        pref = getSharedPreferences("mis_pref", Context.MODE_PRIVATE);
+
+
         btnRegistroGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences.Editor editor = pref.edit();
+
+                String nombre = edtRegistroNombre.getText().toString();
+                String usuario= edtRegistroUsuario.getText().toString();
+                String email= edtRegistroCorreo.getText().toString();
+                String clave= edtRegistroClave.getText().toString();
+                String clave2= edtRegistroClave2.getText().toString();
+
+                editor.putString("nombre",nombre);
+                editor.putString("usuario",usuario);
+                editor.putString("email",email);
+                editor.putString("clave",clave);
+                editor.putString("clavesegunda",clave2);
+
+                editor.commit();
                 finish();
             }
         });
